80c3a56547b3deba89acdfe2a5ccef0286e78ab3
mohamed affes <med123011@gmail.com>
2025-04-09 12:51:11 +0100

    feat(login): improve error handling and user feedback on login attempts

--- src/main/java/org/openjfx/Login.java
+++ src/main/java/org/openjfx/Login.java
@@ -32,30 +32,35 @@ public class Login {
         String emailText = email.getText();
         String passwordText = password.getText();
 
-        if (LoginChecker.loginCheck(emailText, passwordText) == LoginChecker.LoginStatus.NORMAL_USER) {
-            System.out.println("Login successful!");
-            // Load the next scene or perform the next action
-        } else {
-            System.out.println("Login failed. Please check your credentials.");
+        if (emailText.isEmpty() || passwordText.isEmpty()) {
+            System.out.println("Please fill in all fields.");
+            error.setText("Please fill in all fields.");
+            return;
         }
         switch (LoginChecker.loginCheck(emailText, passwordText)) {
             case NORMAL_USER:
                 System.out.println("Login successful! normal user");
+                error.setText("Login successful! normal user");
                 break;
             case ADMIN_USER:
                 System.out.println("Login successful! admin user");
+                error.setText("Login successful! admin user");
                 break;
             case USER_NOT_FOUND:
                 System.out.println("Login failed. User not found.");
+                error.setText("Login failed. User not found.");
                 break;
             case INACTIVE_USER:
                 System.out.println("Login failed. User is inactive.");
+                error.setText("Login failed. User is inactive.");
                 break;
             case CONNEXION_FAILED:
                 System.out.println("Login failed. Connection error.");
+                error.setText("Login failed. Connection error.");
                 break;
             default:
                 System.out.println("Unknown error.");
+                error.setText("Unknown error.");
                 break;
         }
     }

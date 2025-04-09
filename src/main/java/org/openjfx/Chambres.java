ca1a0a57a0a99f42f3208b5c0c65f9ed647de1ad
Viceroy <ayoub.ouali@egg.tn>
2025-04-09 21:46:05 +0100

    feat(rooms): add loading rooms from db for ui

--- src/main/java/org/openjfx/Chambres.java
+++ src/main/java/org/openjfx/Chambres.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 
 import javafx.collections.FXCollections;
 
@@ -11,8 +13,13 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Chambres implements Initializable{
+import org.app.RoomSelect;
+import java.util.ArrayList;
+import org.models.RoomModel;
 
+
+public class Chambres implements Initializable{
+    private ArrayList<RoomModel> rooms;
     @FXML
     private ComboBox<String> filter;
 
@@ -25,10 +32,25 @@ public class Chambres implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 	filter.setItems(FXCollections.observableArrayList("Etage", "Type", "Nombre Personnes", "Prix", "Etat"));
+	
+	list.getChildren().clear();
+
+	try {
+	    rooms = RoomSelect.roomSelect();
+	
+	    for (RoomModel room : rooms) {
+	         System.out.println(room.getIdChambre());
+	    }
+	} catch (Exception e) {
+	    System.out.println("Erreur de connection a la base de donnée");
+	}
     }
     @FXML
-    private void updateList() {
+    private void updateList() throws Exception{
 	System.out.println(filter.getValue());
+	Parent chambre = FXMLLoader.load(getClass().getResource("/chambreButton.fxml"));
+	list.getChildren().clear();
+	list.getChildren().add(chambre);
     }
 
 }

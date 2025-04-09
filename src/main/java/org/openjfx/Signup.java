a17b8f680fb9cfe42e494768aa931b41e80f3158
Viceroy <ayoub.ouali@egg.tn>
2025-04-09 17:04:49 +0100

    feat(signup): add input control to signup

--- src/main/java/org/openjfx/Signup.java
+++ src/main/java/org/openjfx/Signup.java
@@ -6,12 +6,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
 import org.app.UserAdd;
 
 public class Signup {
 
     @FXML
     private Button confirm;
+	
+    @FXML
+    private Label error;
 
     @FXML
     private TextField email;
@@ -33,19 +37,27 @@ public class Signup {
     }
 
     @FXML
-    void submit(ActionEvent event) {
+    void submit(ActionEvent event) throws Exception{
         String nomText = nom.getText();
         String prenomText = prenom.getText();
         String emailText = email.getText();
         String passwordText = password.getText();
-
+	
+	// Test if fields are empty
+	if (nomText.isEmpty() || prenomText.isEmpty() || emailText.isEmpty() || passwordText.isEmpty()) {
+	    error.setText("Veuillez remplir les champs!");
+	    return;
+	}
         // Appel de la méthode userAdd avec les valeurs des champs de texte
         boolean result = UserAdd.userAdd(nomText, prenomText, emailText, passwordText, false, false);
 
         if (result) {
             System.out.println("Utilisateur ajouté avec succès !");
+	    App main = new App();
+	    main.changeScene("/mainMenu.fxml");
         } else {
             System.out.println("Échec de l'ajout de l'utilisateur.");
+	    error.setText("Verifier la connexion ou si vous avez deja un compte!");
         }
     }
 

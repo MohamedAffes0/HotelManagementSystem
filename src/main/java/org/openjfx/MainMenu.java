d16f4de5104d48e8e4d03b6de8e36497d7de60fc
Viceroy <ayoub.ouali@egg.tn>
2025-04-09 15:58:22 +0100

    feat(ui): add main meny screen

---
+++ src/main/java/org/openjfx/MainMenu.java
@@ -0,0 +1,46 @@
+package org.openjfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class MainMenu {
+
+    @FXML
+    private Button chambres;
+
+    @FXML
+    private Button clients;
+
+    @FXML
+    private Button comptes;
+
+    @FXML
+    private ScrollPane contentContainer;
+
+    @FXML
+    private Button reservations;
+
+    @FXML
+    void chambresPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clientsPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comptesPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reservationsPressed(ActionEvent event) {
+
+    }
+
+}
+

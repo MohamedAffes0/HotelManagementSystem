929d5e0a44db6e6b90b99c583eb5dd2ab5d00598
mohamed affes <med123011@gmail.com>
2025-04-09 20:03:07 +0100

    feat(reservation): implement reservation model and procedure to retrieve all reservations

---
+++ plsql/Get_all_reservation.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PROCEDURE get_all_reservation(
+    p_cursor OUT SYS_REFCURSOR
+) AS
+BEGIN
+    OPEN p_cursor FOR
+        SELECT id_reservation, date_debut, date_fin, paid, employe, client_hotel, chambre
+        FROM reservation;
+END;
+/
---
+++ src/main/java/org/app/ReservationSelect.java
@@ -0,0 +1,85 @@
+package org.app;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Date;
+
+import org.database.DBConnect;
+import org.models.ReservationModel;
+
+import oracle.jdbc.OracleTypes;
+
+public class ReservationSelect {
+    public static ArrayList<ReservationModel> userSelect() {
+        Connection connection = null;
+        CallableStatement stmt = null;
+        ArrayList<ReservationModel> reservations = new ArrayList<>();
+        try {
+            connection = DBConnect.connect();
+            if (connection != null) {
+                String sql = "{ call get_all_reservation(?) }";
+                stmt = connection.prepareCall(sql);
+                stmt.registerOutParameter(1, OracleTypes.CURSOR);                
+
+                stmt.execute();
+                ResultSet result = null;
+                try {
+                    result = (ResultSet) stmt.getObject(1);
+                    while (result.next()) {
+                        int id = result.getInt("id_reservation");
+                        Date dateDebut = result.getDate("date_debut");
+                        Date DateFin = result.getDate("date_fin");
+                        boolean isPaid = result.getInt("paid") == 1? true : false;
+                        int employee = result.getInt("employe");
+                        int client = result.getInt("client_hotel");
+                        int chambre = result.getInt("chambre");
+
+                        reservations.add(new ReservationModel(id, dateDebut, DateFin, isPaid, employee, client, chambre));
+                    }
+                } finally {
+                    if (result != null) {
+                        try {
+                            result.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                return reservations;
+            } else {
+                System.err.println("Échec de la connexion à la base de données.");
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            // toujour executer le bloc finally
+            // Fermeture des ressources JDBC
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     ArrayList<ReservationModel> reservations = userSelect();
+    //     for (int i = 0; i < reservations.size(); i++) {
+    //         System.out.println(reservations.get(i).getIdReservation() + " " + 
+    //         reservations.get(i).getDateDebut() + " " + reservations.get(i).getDateFin() 
+    //         + " " + reservations.get(i).isPaid() + " " + reservations.get(i).getEmploye() 
+    //         + " " + reservations.get(i).getClientHotel() + " " + reservations.get(i).getChambre());
+    //     }
+    // }
+}

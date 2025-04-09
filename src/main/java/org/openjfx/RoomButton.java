1ffe1a5aa2eb42217be698cbe28158c0a43a2776
Viceroy <ayoub.ouali@egg.tn>
2025-04-09 22:27:03 +0100

    feat(ui): add room button class

---
+++ src/main/java/org/openjfx/RoomButton.java
@@ -0,0 +1,27 @@
+package org.openjfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class RoomButton {
+
+    @FXML
+    private Label floor;
+
+    @FXML
+    private Label id;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private Label size;
+
+    @FXML
+    private Label state;
+
+    @FXML
+    private Label type;
+
+}
+

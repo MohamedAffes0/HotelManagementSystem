cfb8fb9488bee53b3ef63b1af113480a8b6a4d0f
Viceroy <ayoub.ouali@egg.tn>
2025-04-13 15:30:35 +0100

    feat(signup): add checking email in signup

--- src/main/java/org/openjfx/Signup.java
+++ src/main/java/org/openjfx/Signup.java
@@ -8,62 +8,67 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Label;
 
+import org.app.EmailChecker;
 import org.app.user.UserAdd;
 import org.app.user.UserAdd.CreationStatus;
 import org.models.EmployeeModel;
 
 public class Signup {
 
-    @FXML
-    private Button confirm;
-	
-    @FXML
-    private Label error;
+	@FXML
+	private Button confirm;
 
-    @FXML
-    private TextField email;
+	@FXML
+	private Label error;
 
-    @FXML
-    private TextField nom;
+	@FXML
+	private TextField email;
 
-    @FXML
-    private PasswordField password;
+	@FXML
+	private TextField nom;
 
-    @FXML
-    private TextField prenom;
+	@FXML
+	private PasswordField password;
 
-    @FXML
-    void loadLogin(MouseEvent event) throws Exception{
-	App main = new App();
-	main.changeScene("/login.fxml");
+	@FXML
+	private TextField prenom;
 
-    }
+	@FXML
+	void loadLogin(MouseEvent event) throws Exception {
+		App main = new App();
+		main.changeScene("/login.fxml");
 
-    @FXML
-    void submit(ActionEvent event) throws Exception{
-        String nameText = nom.getText();
-        String lastNameText = prenom.getText();
-        String emailText = email.getText();
-        String passwordText = password.getText();
-	
-	// Test if fields are empty
-    if (nameText.isEmpty() || lastNameText.isEmpty() || emailText.isEmpty() || passwordText.isEmpty()) {
-        error.setText("Veuillez remplir les champs!");
-        return;
 	}
-        // Appel de la méthode userAdd avec les valeurs des champs de texte
-        EmployeeModel user = new EmployeeModel(1, nameText, lastNameText, emailText, passwordText, false, false);
-        CreationStatus result = UserAdd.userAdd(user);
 
-        if (result == CreationStatus.SUCCESS) {
-            System.out.println("Utilisateur ajouté avec succès !");
-            App main = new App();
-            main.changeScene("/confirmationPending.fxml");
-        } else {
-            System.out.println("Échec de l'ajout de l'utilisateur.");
-            error.setText("Verifier la connexion ou si vous avez deja un compte!");
-        }
-    }
+	@FXML
+	void submit(ActionEvent event) throws Exception {
+		String nameText = nom.getText();
+		String lastNameText = prenom.getText();
+		String emailText = email.getText();
+		String passwordText = password.getText();
+
+		// Test if fields are empty
+		if (nameText.isEmpty() || lastNameText.isEmpty() || emailText.isEmpty() || passwordText.isEmpty()) {
+			error.setText("Veuillez remplir les champs!");
+			return;
+		}
+
+		if (EmailChecker.isValid(emailText)) {
+			error.setText("Email Invalide");
+		}
+		// Appel de la méthode userAdd avec les valeurs des champs de texte
+		EmployeeModel user = new EmployeeModel(1, nameText, lastNameText, emailText, passwordText, false,
+				false);
+		CreationStatus result = UserAdd.userAdd(user);
+
+		if (result == CreationStatus.SUCCESS) {
+			System.out.println("Utilisateur ajouté avec succès !");
+			App main = new App();
+			main.changeScene("/confirmationPending.fxml");
+		} else {
+			System.out.println("Échec de l'ajout de l'utilisateur.");
+			error.setText("Verifier la connexion ou si vous avez deja un compte!");
+		}
+	}
 
 }
-

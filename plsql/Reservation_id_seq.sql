e86796bb5b2020efc3a1785a297b4c049054a7c1
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:28:14 +0200

    chore(plsql): add explanatory comments to 'Reservation_id_seq.sql'

--- plsql/Reservation_id_seq.sql
+++ plsql/Reservation_id_seq.sql
@@ -1,5 +1,6 @@
 CREATE SEQUENCE reservation_id_seq
+  --generating automatic reservation ids for the reservations--
 START WITH 1
 INCREMENT BY 1
 NOCACHE
-NOCYCLE;
+NOCYCLE --so that it doesn't return to 1 if the maximum value is reached--;

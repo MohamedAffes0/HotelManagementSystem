7c9ce96d328d449a01f9b5f3ecc5d4ddf3b41abf
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:26:41 +0200

    chore(plsql): add explanatory comments to 'Modify_user.sql'

--- plsql/Modify_user.sql
+++ plsql/Modify_user.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE PROCEDURE modify_user(
-    p_id IN NUMBER,
-    p_admin IN NUMBER DEFAULT 0,
-    p_active IN NUMBER DEFAULT 0
+    p_id IN NUMBER,--id of the user--
+    p_admin IN NUMBER DEFAULT 0,--equals 1 if the user is an admin--
+    p_active IN NUMBER DEFAULT 0--equals 1 if the account is active--
 ) AS
 BEGIN
+    --update the 'is admin' and 'is_active' attributes for the user with p_id--
     UPDATE employe 
     set is_admin = p_admin, is_active = p_active
     WHERE id = p_id;

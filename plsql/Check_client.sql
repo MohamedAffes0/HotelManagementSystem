729930f8590871bdd9aab7262df805c1104cdfd8
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 22:48:17 +0200

    chore(plsql): add explanatory comments to 'Check_client.sql'

--- plsql/Check_client.sql
+++ plsql/Check_client.sql
@@ -1,8 +1,9 @@
 CREATE OR REPLACE PROCEDURE check_client (
-    p_cin IN NUMBER,
-    p_exists OUT NUMBER
+    p_cin IN NUMBER,--the id of the client--
+    p_exists OUT NUMBER--equals 1 if the client exits, else 0--
 ) AS
 BEGIN
+    --check if the client exists in the database--
     SELECT COUNT(*) INTO p_exists FROM client_hotel
     WHERE cin = p_cin;
     IF p_exists > 0 THEN

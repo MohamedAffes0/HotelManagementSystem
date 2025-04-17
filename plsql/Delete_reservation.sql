a36d45dd6f5671df1984c1d1c9e3a6223b42ceae
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:11:52 +0200

    chore(plsql): add explanatory comments to 'Delete_reservation.sql'

--- plsql/Delete_reservation.sql
+++ plsql/Delete_reservation.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE PROCEDURE delete_reservation(
-    p_id IN NUMBER
+    p_id IN NUMBER --id of the reservation--
 ) AS
 BEGIN
+    --delete the reservation with p_id from the database--
     DELETE FROM reservation
     WHERE id_reservation = p_id;
     COMMIT;

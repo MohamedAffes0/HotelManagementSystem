7c95a02dd639314732eccb952a797d8e3f85be72
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:10:04 +0200

    chore(plsql): add explanatory comments to 'Delete_client.sql'

--- plsql/Delete_client.sql
+++ plsql/Delete_client.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE PROCEDURE delete_client(
-    p_cin IN NUMBER
+    p_cin IN NUMBER --id of the client--
 ) AS
 BEGIN
+    --delete the client with p_cin from the database--
     DELETE FROM client_hotel 
     WHERE cin = p_cin;
     COMMIT;

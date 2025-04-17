1705e837b5ac03c4a3681616d37f7be5a2e5d211
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:19:26 +0200

    chore(plsql): add explanatory comments to 'Get_all_reservations.sql'

--- plsql/Get_all_reservation.sql
+++ plsql/Get_all_reservation.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE PROCEDURE get_all_reservation(
-    p_cursor OUT SYS_REFCURSOR
+    p_cursor OUT SYS_REFCURSOR --cursor that will dynamically return the data regarding the reservations--
 ) AS
 BEGIN
     OPEN p_cursor FOR

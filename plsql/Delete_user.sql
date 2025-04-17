9957bd4190410700191100ac22da5aa9b673f5c8
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:12:56 +0200

    chore(plsql): add explanatory comments to 'Delete_user.sql'

--- plsql/Delete_user.sql
+++ plsql/Delete_user.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE PROCEDURE delete_user(
-    p_id IN NUMBER
+    p_id IN NUMBER --id of the user--
 ) AS
 BEGIN
+    --delete the employe with p_id from the database--
     DELETE FROM employe 
     WHERE id = p_id;
     COMMIT;

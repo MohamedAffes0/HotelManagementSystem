1cbd7fd9a3b1d371e7813a109091c18bd50c94d2
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:05:38 +0200

    chore(plsql): add explanatory comments to 'Check_login.sql'

--- plsql/Check_login.sql
+++ plsql/Check_login.sql
@@ -1,16 +1,18 @@
 CREATE OR REPLACE PROCEDURE check_login (
-    p_email IN VARCHAR2,
-    p_password IN VARCHAR2,
-    p_exists OUT NUMBER,
-    p_active OUT NUMBER,
-    p_admin OUT NUMBER
+    p_email IN VARCHAR2,--email of the employe--
+    p_password IN VARCHAR2,--password of the employe--
+    p_exists OUT NUMBER,--equals 1 if the employe exists--
+    p_active OUT NUMBER,--equals 1 if the account is active--
+    p_admin OUT NUMBER--equals 1 if the employe is an admin--
 ) AS
 BEGIN
     BEGIN
+    --check if the employe exists in the database--
         SELECT COUNT(*) INTO p_exists
         FROM EMPLOYE
         WHERE mail = p_email AND mdp = p_password;
-        
+
+        --check if the employe is an admin and if the account is active--
         SELECT is_admin, is_active INTO p_admin, p_active
         FROM EMPLOYE
         WHERE mail = p_email AND mdp = p_password;
@@ -26,6 +28,7 @@ BEGIN
             p_exists := 0;
             p_active := 0;
             p_admin := 0;
+--handle other possible exceptions--
         WHEN OTHERS THEN
             p_exists := 0;
             p_active := 0;

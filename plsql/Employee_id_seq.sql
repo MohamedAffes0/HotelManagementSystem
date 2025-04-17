d290dbc98031c152524729b096273eef08b4cdd7
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:16:42 +0200

    chore(plsql): add explanatory comments to 'Employee_id_seq.sql'

--- plsql/Employee_id_seq.sql
+++ plsql/Employee_id_seq.sql
@@ -1,5 +1,6 @@
 CREATE SEQUENCE employe_id_seq
+  --generate automatic ids for the employees--
 START WITH 1
 INCREMENT BY 1
 NOCACHE
-NOCYCLE;
+NOCYCLE --so that it doesn't restart from 1 if the maximum value is reached--;

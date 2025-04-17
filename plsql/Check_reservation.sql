94ecf1d9a9c22dc2d01b0d996e8bb6bbb6b66d6f
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-04-17 23:08:51 +0200

    chore(plsql): add explanatory comments to 'Check_reservation.sql'

--- plsql/Check_reservation.sql
+++ plsql/Check_reservation.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE PROCEDURE check_reservation (
-    p_id_chambre IN NUMBER,
-    p_cursor OUT SYS_REFCURSOR
+    p_id_chambre IN NUMBER,--id of the room--
+    p_cursor OUT SYS_REFCURSOR--the cursor that will return the data regarding the reservation dynamically--
 ) AS
 BEGIN
     OPEN p_cursor FOR
+    --check if a reservation for a certain room during a certain period has already been made--
         SELECT date_debut, date_fin
         FROM reservation
         WHERE chambre = p_id_chambre;

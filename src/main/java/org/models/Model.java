93500faa12d0628688fad789ea6192bde898f75a
Viceroy <ayoub.ouali@egg.tn>
2025-04-19 17:45:45 +0100

    feat(models)!: add getdata abstract method

--- src/main/java/org/models/Model.java
+++ src/main/java/org/models/Model.java
@@ -12,5 +12,5 @@ public abstract class Model {
 	// Returns true if this object fits the filter criterea.
 	public abstract boolean filter(TextField search, String filterType);
 
-	public ArrayList<Model> getData() {return null;}
+	public abstract ArrayList<Model> getData() {return null;}
 }

f3498be30ec5a6916a87747ec5b0e418a1d8691a
Viceroy <ayoub.ouali@egg.tn>
2025-04-26 19:57:41 +0100

    feat(ui): add icons to list button

--- src/main/java/org/models/Model.java
+++ src/main/java/org/models/Model.java
@@ -3,6 +3,8 @@ package org.models;
 import java.util.ArrayList;
 
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 
 /**
  * Model: base class for all models files.
@@ -13,4 +15,15 @@ public abstract class Model {
 	public abstract boolean filter(TextField search, String filterType);
 
 	public abstract ArrayList<ModelField> getFields();
+
+	public SVGPath createIcon(String svgPath, double scale, String colorHex) {
+		SVGPath icon = new SVGPath();
+		icon.setContent(svgPath);
+		icon.setScaleX(scale);
+		icon.setScaleY(scale);
+		icon.setFill(Color.web(colorHex));
+		//button.setGraphic(icon);
+		//button.setContentDisplay(ContentDisplay.LEFT);
+		return icon;
+	}
 }

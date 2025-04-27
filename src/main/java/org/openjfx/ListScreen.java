5373e19cfc6f193efcdbc8790046f7c9a77ff340
mohamed affes <med123011@gmail.com>
2025-04-27 10:13:44 +0100

    feat(ui): refactor button icon setting and update styles for delete and logout buttons

--- src/main/java/org/openjfx/ListScreen.java
+++ src/main/java/org/openjfx/ListScreen.java
@@ -141,6 +141,7 @@ public class ListScreen<T extends Model, L extends DBLoader> {
 
 	public void setAddButtonText(String text) {
 		addButton.setText(text);
+		UiUtils.setIconToButton(addButton, "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" + "M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4", 1, "#2f2f2f");
 	}
 
 	public void setAddPopup(AddPopup addPopup) {

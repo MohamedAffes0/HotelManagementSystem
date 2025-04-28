1b85c9c6b1c1aeb05a6f848f4097247b824cfd63
Viceroy <ayoub.ouali@egg.tn>
2025-04-28 14:51:23 +0100

    refactor(views): refactor views to use new controller

--- src/main/java/org/views/Login.java
+++ src/main/java/org/views/Login.java
@@ -1,7 +1,7 @@
 package org.views;
 
-import org.controllers.EmailChecker;
-import org.controllers.auth.LoginChecker;
+import org.controllers.Controller;
+import org.controllers.exceptions.ControllerException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,80 +13,60 @@ import javafx.scene.input.MouseEvent;
 
 public class Login {
 
-    @FXML
-    private TextField email;
+	@FXML
+	private TextField email;
 
-    @FXML
-    private PasswordField password;
+	@FXML
+	private PasswordField password;
 
-    @FXML
-    private Label error;
+	@FXML
+	private Label error;
 
-    @FXML
-    private Label signup;
+	@FXML
+	private Label signup;
 
-    @FXML
-    private Button submit;
+	@FXML
+	private Button submit;
 
-    @FXML
-    void loginPressed(ActionEvent event) throws Exception{
-        String emailText = email.getText();
-        String passwordText = password.getText();
-        App main = new App();
+	@FXML
+	void loginPressed(ActionEvent event) throws Exception {
+		String emailText = email.getText();
+		String passwordText = password.getText();
+		App main = new App();
 
-        email.getStyleClass().remove("text-field-error");
-        password.getStyleClass().remove("text-field-error");
-        if (emailText.isEmpty() || passwordText.isEmpty()) {
-            System.out.println("Please fill in all fields.");
-            error.setText("Veuillez saisir votre email et votre mot de passe.");
-            email.getStyleClass().add("text-field-error");
-            password.getStyleClass().add("text-field-error");
-            return;
-        }
-	
-        if (!EmailChecker.isValid(emailText)) {
-                error.setText("Email invalide.");
-                email.getStyleClass().add("text-field-error");
-                // password.getStyleClass().add("text-field");
-                return;
-        }
+		try {
+			switch (Controller.getInstance().getUserManager().checkLogin(emailText, passwordText)) {
+				case NORMAL_USER:
+					System.out.println("Login successful! normal user");
+					main.changeScene("/mainMenu.fxml");
+					App.isAdminUser = false;
+					break;
+				case ADMIN_USER:
+					System.out.println("Login successful! admin user");
+					main.changeScene("/mainMenu.fxml");
+					App.isAdminUser = true;
+					break;
+				case USER_NOT_FOUND:
+					System.out.println("Login failed. User not found.");
+					error.setText("Email ou mot de passe incorrecte.");
+					email.getStyleClass().add("text-field-error");
+					password.getStyleClass().add("text-field-error");
+					break;
+				case INACTIVE_USER:
+					System.out.println("Login failed. User is inactive.");
+					main.changeScene("/confirmationPending.fxml");
+					break;
+				default:
+					break;
+			}
+		} catch (ControllerException exception) {
+			error.setText(exception.toString());
+		}
+	}
 
-        switch (LoginChecker.loginCheck(emailText, passwordText)) {
-            case NORMAL_USER:
-                System.out.println("Login successful! normal user");
-                main.changeScene("/mainMenu.fxml");
-                App.isAdminUser = false;
-                break;
-            case ADMIN_USER:
-                System.out.println("Login successful! admin user");
-                main.changeScene("/mainMenu.fxml");
-                App.isAdminUser = true;
-                break;
-            case USER_NOT_FOUND:
-                System.out.println("Login failed. User not found.");
-                error.setText("Email ou mot de passe incorrecte.");
-                email.getStyleClass().add("text-field-error");
-                password.getStyleClass().add("text-field-error");
-                break;
-            case INACTIVE_USER:
-                System.out.println("Login failed. User is inactive.");
-                main.changeScene("/confirmationPending.fxml");
-                break;
-            case CONNEXION_FAILED:
-                System.out.println("Login failed. Connection error.");
-                error.setText("Erreur de connexion.");
-                break;
-            default:
-                System.out.println("Unknown error.");
-                error.setText("Erreur.");
-                break;
-        }
-    }
-
-
-    @FXML
-    void loadSignup(MouseEvent event) throws Exception {
-        App main = new App();
-        main.changeScene("/signup.fxml");
-    }
+	@FXML
+	void loadSignup(MouseEvent event) throws Exception {
+		App main = new App();
+		main.changeScene("/signup.fxml");
+	}
 }
